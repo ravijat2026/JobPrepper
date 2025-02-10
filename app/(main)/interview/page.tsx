b68960ec869c2aa1f3073c7169617b1402a7d082
ravijat2026 <ravijat2026@gmail.com>
2025-02-11 01:32:45 +0530

    quiz

--- app/(main)/interview/page.tsx
+++ app/(main)/interview/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { getAssessments } from "@/actions/interview";
 
-const page = () => {
-  return (
-    <div className='flex h-screen justify-center items-center'>Interview</div>
-  )
+export default async function InterviewPrepPage(){
+    const assessments = await getAssessments();
+
+    return(
+        <div>
+            <div className="flex items-center justify-between mb-5">
+                <h1 className="text-6xl font-bond"> 
+                    Interview Preparation
+                </h1>
+            </div>
+        </div>
+    )
+
 }
-
-export default page
